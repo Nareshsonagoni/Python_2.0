3419f6a7ffc1fb6112a6321110e3d68a406535e3
Nikhil Arram <nikhilarram@Nikhils-Air.modem>
2019-11-21 20:00:06 +1100

    Polymorphism

--- app.py
+++ app.py
@@ -1,59 +1,29 @@
-# In order to create a common interface between different streams, we need to use abstract base class.
-# It will provide some common code to its derivates.
+# Polymorphism ## Many forms
 
 from abc import ABC, abstractmethod
 
-# Simple inheritance example
 
-''' Here we are dealing with stream of data, opening from different sources.
-like network, file from pc, etc..'''
-
-
-class InvalidOperationError(Exception):
-    pass
-
-
-class Stream(ABC):
-    def __init__(self):
-        self.opened = False
-
-    def open(self):
-        if self.opened:
-            # here if the file is already opened we are throwing an error. to do this we need to create a custom Exception.
-            raise InvalidOperationError("Stream already opened")
-        self.opened = True
-
-    def close(self):
-        if not self.opened:
-            # here if the file is not opened we are throwing an error. to do this we need to create a custom Exception.
-            raise InvalidOperationError("Stream not opened")
-        self.opened = False
-
+class UIControl(ABC):
     @abstractmethod
-    def read(self):
+    def draw(self):
         pass
 
 
-class FileStream(Stream):
-    def read(self):
-        print("Reading from the file stream")
+class TextBox(UIControl):
+    def draw(self):
+        print("TextBox")
 
 
-class NetworkStream(Stream):
-    def read(self):
-        print("Reading from the Network stream")
+class DropDownList(UIControl):
+    def draw(self):
+        print("DropDownList")
 
 
-class MemoryStream(Stream):
-    def read(self):
-        print("Reading from a memory stream")
+def draw(controls):
+    for control in controls:
+        control.draw()
 
 
-# We should keep the inheritance to 2 to 3 levels, behond that things will get complicated.
-
-# stream = Stream() We can instance an abstract base class, like a normal class
-memory = MemoryStream()
-memory.read()
-
-#
-#
+ddl = DropDownList()
+ttb = TextBox()
+draw([ddl, ttb])
