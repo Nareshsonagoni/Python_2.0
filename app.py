fcdd82e6674cfee264bc5d275e0903650e9a35bc
Nikhil Arram <nikhilarram@Nikhils-Air.modem>
2019-11-21 18:42:02 +1100

    Simple Inheritance

--- app.py
+++ app.py
@@ -1,20 +1,40 @@
-class Employee:
-    def greet(self):
-        print("Hello Employee")
+# Simple inheritance example
+
+''' Here we are dealing with stream of data, opening from different sources.
+like network, file from pc, etc..'''
 
 
-class Person:
-    def greet(self):
-        print("Hello person")
-
-
-class Manager(Person, Employee):
+class InvalidOperationError(Exception):
     pass
 
 
-# manager object takes the first parent class look through for the method specified,
-manager = Manager()
-# If can't find in the first then it will look in the other parent class.
-manager.greet()
-# It is always good to have diff methods in the parent classes so that it won't create a mess.
-#
+class Stream:
+    def __init__(self):
+        self.opened = False
+
+    def open(self):
+        if self.opened:
+            # here if the file is already opened we are throwing an error. to do this we need to create a custom Exception.
+            raise InvalidOperationError("Stream already opened")
+        self.opened = True
+
+    def close(self):
+        if not self.opened:
+            # here if the file is not opened we are throwing an error. to do this we need to create a custom Exception.
+            raise InvalidOperationError("Stream not opened")
+        self.opened = False
+
+
+class FileStream(Stream):
+    def read(self):
+        print("Reading from the file stream")
+
+
+class NetworkStream(Stream):
+    def read(self):
+        print("Reading from the Network stream")
+
+# We should keep the inheritance to 2 to 3 levels, behond that things will get complicated.
+
+
+file_stream = FileStream()
